45a31f443fdeedcb5d675b78acc03cc2b1c56094
tkorignal <tauqueer.khan@gmail.com>
2019-01-14 22:47:45 +0000

    Fully setup getReop and authentication

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,19 @@
 const request = require('request');
+const {GITHUB_TOKEN} = require('./secret');
 
 console.log(`Welcome to the GitHub Avatar Downloader!`);
 
 function getRepoContributors (repoOwner, repoName, callback) {
-  request(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, 
-    callback);
+  const options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'token ' + GITHUB_TOKEN
+    }
+  }
+  request( options,(err, res, body) => {
+    callback(err, body)
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(error, response) {
