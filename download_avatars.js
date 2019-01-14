994a11ae8fbdb177ecd73e653801dd993b54b696
tkorignal <tauqueer.khan@gmail.com>
2019-01-14 22:19:54 +0000

    Added the getReportContributors functoin;

--- download_avatars.js
+++ download_avatars.js
@@ -2,3 +2,12 @@ const request = require('request');
 
 console.log(`Welcome to the GitHub Avatar Downloader!`);
 
+function getRepoContributors (repoOwner, repoName, callback) {
+  request(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, 
+    callback);
+}
+
+getRepoContributors("jquery", "jquery", function(error, response) {
+  console.log('Errors:', error);
+  console.log('Response:', response);
+});
